3aa1a6faba798f71965b53fe7b66b3d3c4abf5ec
Matt Jackson <matt.jackson@credit-suisse.com>
2012-07-24 15:53:58 +0200

    added token lookup - converted seqs to lists

--- configManager.Unit.Tests/findConfigs.fs
+++ configManager.Unit.Tests/findConfigs.fs
@@ -19,16 +19,17 @@ type configManager() =
         Array.append configs tokens
 
 [<TestFixture>] 
-type ``finding config files`` ()=
+module ``finding config files`` =
     let configManager = new configManager()
-    [<Test>] member test.
-        ``should find tokens file`` ()=
+
+    [<Test>] 
+    let ``should find tokens file`` ()=
         configManager.findConfigs "." |> should contain @".\testFiles\test.tokens.config"
-    [<Test>] member test.
-        ``should find master file`` ()=
+    [<Test>] 
+    let ``should find master file`` ()=
         configManager.findConfigs "." |> should contain @".\testFiles\test.master.config"
-    [<Test>] member test.
-        ``should find global tokens file`` ()=
+    [<Test>] 
+    let ``should find global tokens file`` ()=
         configManager.findConfigs "." |> should contain @".\testFiles\global.tokens"
 
 open System.Yaml
@@ -36,26 +37,30 @@ open System.Collections.Generic
 
 type token = {
     name : string
-    envs : seq<(string * string)>
+    envs : (string * string) list
     }
 
 type yamlReader() =
     member this.read file =
-        Seq.ofArray<YamlNode>(YamlNode.FromYamlFile(file)) 
-        |> Seq.head<YamlNode> :?> YamlMapping
+        List.ofArray<YamlNode>(YamlNode.FromYamlFile(file)) 
+        |> List.head<YamlNode> :?> YamlMapping
     member this.toTokens (yamlDoc : YamlMapping) =
-        seq { 
+        [
             for configSetting in yamlDoc.Keys do
             yield { 
                 name = (configSetting :?> YamlScalar).Value; 
-                envs = seq { 
+                envs = 
+                [
                     for token in yamlDoc.[configSetting] :?> YamlMapping do
                         let env = (token.Key :?> YamlScalar).Value
                         let value = (token.Value :?> YamlScalar).Value
                         yield (env, value)
-                } 
+                ]
             } 
-        }   
+        ]
+    member this.lookup (tokens:token list) (token:string) (env:string) =
+        let tokenValues = tokens |> List.find(fun (t : token) -> t.name = token)
+        tokenValues.envs |> List.find(fun (e : (string * string)) -> (fst e) = env) |> snd
 
 [<TestFixture>] 
 module ``reading yaml files`` =
@@ -70,12 +75,17 @@ module ``reading yaml files`` =
         let token1 = yamlConfig.[new YamlScalar("token1")] :?> YamlMapping
         let env3 = token1.[new YamlScalar("env3")] :?> YamlScalar
         env3.Value |> should equal "value3"
-    [<Test>] 
+    [<Test>]
     let ``should convert yamldocument to digestable format`` ()=
         let tokens = yamlReader.toTokens yamlConfig
         let firstToken = (Seq.head tokens)
         firstToken.name |> should equal "token2"
         Seq.head(firstToken.envs) |> snd |> should equal "value2"
+    [<Test>]
+    let ``should be able to look up token easily`` () =
+        let tokens = yamlReader.toTokens yamlConfig
+        let tokenValue = yamlReader.lookup tokens "token2" "env3"
+        tokenValue |> should equal "value3"
 
 open System.Text.RegularExpressions
 
@@ -88,12 +98,13 @@ type sunshineConfig ()=
         "value3"
 
 [<TestFixture>] 
-type ``substituting tokens`` ()=
+module ``substituting tokens`` =
     let yamlReader = new yamlReader()
     let tokens = yamlReader.read("./testFiles/config.yaml")
     let sunshineConfig = new sunshineConfig()
-    [<Test>] member test.
-        ``finding a token should replace it from the sunshineMapping`` ()=
-        let masterConfig = "<add name=\"Sunshine\" connectionString=\"$$token2$$\"/>" in
-        let swappedConfig = sunshineConfig.swapTokens masterConfig tokens "env3" in
+
+    [<Test>] [<Ignore("pending")>]
+    let ``finding a token should replace it from the sunshineMapping`` ()=
+        let masterConfig = "<add name=\"Sunshine\" connectionString=\"$$token2$$\"/>" 
+        let swappedConfig = sunshineConfig.swapTokens masterConfig tokens "env3" 
         swappedConfig |> should equal "<add name=\"Sunshine\" connectionString=\"value3\"/>"
